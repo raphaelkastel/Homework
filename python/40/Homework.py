5dcc86ffca6f3e0cfc1710328646e544c8672d38
Raphael Kastel <raphykastel@gmail.com>
2019-04-12 17:33:52 -0400

    👎🕮👎 ♓⬧ ♑❒♏♋⧫📬

---
+++ python/40/Homework.py
@@ -0,0 +1,20 @@
+
+def sort(nums):
+
+    for i in range(len(nums)-1):
+        minpos = i
+        for j in range(i, len(nums)):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+    return nums
+
+
+nums = [5, 10, 15, 7, 32, 27, 2, 67]
+nums2 = [2, 7, 26, 32, 13, 5, 46, 32, 3, 76, 4]
+sorted = sort(nums)
+print(sort(nums2))
+print(sorted)
