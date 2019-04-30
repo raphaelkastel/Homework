999a138aa9affef18c5440e06d0ce3a977142974
Raphael Kastel <raphykastel@gmail.com>
2019-04-30 19:00:32 -0400

    ❄♒♓⬧ ♓⬧ ⬥♒⍓ ⍓□◆ ♎□■🕯⧫ ♑♓❖♏ ❖♋♑◆♏ ♋⬧⬧♓♑■❍♏■⧫⬧📬

--- python/40/Homework.py
+++ python/40/Homework.py
@@ -1,3 +1,5 @@
+import random
+
 
 def sort(nums):
 
@@ -18,3 +20,15 @@ nums2 = [2, 7, 26, 32, 13, 5, 46, 32, 3, 76, 4]
 sorted = sort(nums)
 print(sort(nums2))
 print(sorted)
+
+
+def die(x):
+    return random.randint(1, x)
+
+
+def diesix():
+    return random.randint(1, 6)
+
+
+print(die(6))
+print(diesix())
