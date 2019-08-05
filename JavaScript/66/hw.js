4dd67f0eb1c0cb07a8d11fd27fe2e71d9bc4bf10
Raphael Kastel <raphykastel@gmail.com>
2019-08-05 12:24:15 -0400

    Faketube, totally not a ripoff.

---
+++ JavaScript/66/hw.js
@@ -0,0 +1,62 @@
+/*global $*/
+(function () {
+    'use strict';
+    const playing = $('#play');
+    const videostoplay = $('#videos');
+    playing.hide();
+    fetch("videos.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(rt => {
+            videostoplay.append(`Choose a video: `);
+            for (let i = 0; i < rt.length; i++) {
+                const elem = Object.values(rt[i]);
+                if(elem[1] === ""){
+                    elem[1] = elem[4];
+                }
+                videostoplay.append(`<div class="video" id = ${elem[3]} ><img src="${elem[1]}" alt="img"><div>${elem[0]}</div></div> `);
+            
+        }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .then(rt => {
+            $(".video").click((e) => {
+                playing.empty();
+
+                fetch("videos.json")
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`${response.status} ${response.statusText}`);
+                        }
+                        return response.json();
+                    })
+                    .then(rt => {
+                        let id = $(e.currentTarget).attr('id');
+                        for (let i = 0; i < rt.length; i++) {
+                            const elem = Object.values(rt[i]);
+
+                            if (id == rt[i].id) {
+                                 playing.append(`<object type="application/x-shockwave-flash" style="width:450px; height:366px;" data="//www.youtube.com/v/${elem[2]}?color2=FBE9EC&amp;rel=0&amp;showsearch=0&amp;showinfo=0&amp;version=3&amp;modestbranding=1">
+                                <param name="movie" value="//www.youtube.com/v/${elem[2]}?color2=FBE9EC&amp;rel=0&amp;showsearch=0&amp;showinfo=0&amp;version=3&amp;modestbranding=1" />
+                                <param name="allowFullScreen" value="true" />
+                                <param name="allowscriptaccess" value="always" />
+                                </object>
+                            <h1>${elem[0]}</h1>`);
+
+                                playing.show();
+                            }
+
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            });
+        })
+}());
