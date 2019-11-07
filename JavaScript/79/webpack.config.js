42caaa4a4f9a6a26d4e071cc0a195e2c49772cb3
Raphael Kastel <raphykastel@gmail.com>
2019-11-07 12:57:12 -0500

    Diarrhea

--- JavaScript/79/webpack.config.js
+++ JavaScript/79/webpack.config.js
@@ -1,31 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    /*entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },*/
+  
     entry: {
         app: './src/index.js',
-        sayHello: './src/sayHello.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
     plugins: [
-        new CompressionPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'PCS Webpack Page',
             template: './src/index.html'
         }),
-        new webpack.BannerPlugin("banner")
+        new webpack.BannerPlugin('Copyright 2019 PCS'),
+        new CompressionPlugin(),
+        new webpack.ProgressPlugin((percentage, message, ...args) => {
+            console.info(percentage, message, ...args);
+        })
     ],
     module: {
         rules: [
@@ -35,15 +43,7 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
-            },/*
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            }*/
+            },
             {
                 test: /\.(png|jpg|gif)$/i,
                 use: [
@@ -54,15 +54,17 @@ module.exports = {
                         },
                     },
                 ],
-            }/*,
+            },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-                options: {
-                    // eslint options (if necessary)
-                },
-            }*/
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
         ]
     }
 };
